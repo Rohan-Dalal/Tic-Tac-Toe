5f35e7cd74e2a4afe3755e8aca6be7bb59156469
Rohan Dalal <rohan.d.dalal@gmail.com>
2024-08-04 20:19:58 -0400

    Project Complete

--- javascript.js
+++ javascript.js
@@ -1,25 +1,40 @@
 // Game Board Object
 const gameBoard = (function() {
+    let freezeBoard = false;
+
     const boardArray = [
         [undefined, undefined, undefined],
         [undefined, undefined, undefined],
         [undefined, undefined, undefined]
     ];
 
-    const updateBoard = function(row, column, player) {
-        boardArray[row][column] = player;
+    const updateBoard = function(row, column, player, space) {
+        if(!freezeBoard && !boardArray[row][column]){
+            boardArray[row][column] = player;
+            let img = document.createElement("img");
+            img.setAttribute("src", player.image);
+            space.appendChild(img);
 
-        gameFlow.checkWin(row, column, player);
-        gameFlow.updatePlays();
-        gameFlow.playersTurn();
+            if(gameFlow.checkWin(row, column, player)) {
+                freezeBoard = true;
+            } else {
+                gameFlow.updatePlays();
+                gameFlow.playersTurn();
+            }
+        }
     };
 
     const restartBoard = function() {
         boardArray[0] = [undefined, undefined, undefined];
         boardArray[1] = [undefined, undefined, undefined];
         boardArray[2] = [undefined, undefined, undefined];
+
+        winDisplay.innerText = "Who Will Win?!";
+        
         gameFlow.restartPlays();
         gameFlow.playersTurn();
+
+        freezeBoard = false;
     };
 
     return {boardArray, updateBoard, restartBoard};
@@ -27,13 +42,9 @@ const gameBoard = (function() {
 
 // Game Flow Object
 const gameFlow = (function() {
-    let plays = 0;
+    let plays = 1;
 
     const checkWin = function(row, column, player) {
-        if(plays === 9) {
-            return undefined;
-        }
-
         const boardArray = gameBoard.boardArray;
 
         const win = ((boardArray[row][0] === player && boardArray[row][1] === player && boardArray[row][2] === player) ||
@@ -43,12 +54,15 @@ const gameFlow = (function() {
         if (win === true) {
             updateScores(player);
         }
-        return win;
+        if(plays !== 9) {
+            return win;
+        }
+        winDisplay.innerText = "Tie :(";
     };
 
     const updatePlays = () => plays++;
 
-    const restartPlays = () => plays = 0;
+    const restartPlays = () => plays = 1;
 
     const playersTurn = function() {
         const playersTurn = document.querySelector("#turn-display");
@@ -63,16 +77,15 @@ const gameFlow = (function() {
     };
 
     const updateScores = function(player) {
-            let winDisplay = document.querySelector("#display-win p");
             winDisplay.innerText = `${playersTurn().name} Won!`
 
             if(player === playerOne) {
                 playerOne.updateWins();
-                let playerOneScoreDisplay = document.querySelector("#score-display div:nth-child(1) p:nth-child(2)");
+                let playerOneScoreDisplay = document.querySelector("#score-displays div:nth-child(1) p:nth-child(2)");
                 playerOneScoreDisplay.innerText = `${playerOne.getWins()}`;
             } else {
                 playerTwo.updateWins();
-                let playerTwoScoreDisplay = document.querySelector("#score-display div:nth-child(2) p:nth-child(2)");
+                let playerTwoScoreDisplay = document.querySelector("#score-displays div:nth-child(2) p:nth-child(2)");
                 playerTwoScoreDisplay.innerText = `${playerTwo.getWins()}`;
             }
     };
@@ -81,13 +94,13 @@ const gameFlow = (function() {
 })();
 
 // Create Player Function Factory
-function createPlayer(name) {
+function createPlayer(name, image) {
     let wins = 0;
 
     const getWins = () => wins;
     const updateWins = () => wins++;
 
-    return {name, getWins, updateWins};
+    return {name, getWins, updateWins, image};
 }
 
 let playerForm = document.querySelector("#player-creation");
@@ -100,52 +113,45 @@ let submitPlayers = document.querySelector("#submit");
 let playerOneInput = document.querySelector("#player-one");
 let playerTwoInput = document.querySelector("#player-two");
 submitPlayers.addEventListener("click", (e) => {
-    if(playerOneInput.value == "") {
-        playerOneInput.value = "Player One";
-    }
-    if(playerTwoInput.value == "") {
-        playerTwoInput.value = "Player Two";
-    }
+if(playerOneInput.value == "") {
+    playerOneInput.value = "Player One";
+}
+if(playerTwoInput.value == "") {
+    playerTwoInput.value = "Player Two";
+}
 
-    playerOne = createPlayer(playerOneInput.value);
-    playerTwo = createPlayer(playerTwoInput.value);
+playerOne = createPlayer(playerOneInput.value, "images\\o.png");
+playerTwo = createPlayer(playerTwoInput.value, "images\\x.png");
 
-    let playerOneDisplay = document.querySelector("#player-display div:nth-child(1) p");
-    playerOneDisplay.innerText = `Player One: ${playerOne.name}`;
-    let playerTwoDisplay = document.querySelector("#player-display div:nth-child(2) p");
-    playerTwoDisplay.innerText = `Player Two: ${playerTwo.name}`;
+let playerOneDisplay = document.querySelector("#player-displays div:nth-child(1) p");
+playerOneDisplay.innerText = `Player One: ${playerOne.name}`;
+let playerTwoDisplay = document.querySelector("#player-displays div:nth-child(2) p");
+playerTwoDisplay.innerText = `Player Two: ${playerTwo.name}`;
 
-    let playerOneScoreDisplay = document.querySelector("#score-display div:nth-child(1) p:nth-child(1)");
-    playerOneScoreDisplay.innerText = `Player One: ${playerOne.name}`;
-    let playerTwoScoreDisplay = document.querySelector("#score-display div:nth-child(2) p:nth-child(1)");
-    playerTwoScoreDisplay.innerText = `Player Two: ${playerTwo.name}`;
+let playerOneScoreDisplay = document.querySelector("#score-displays div:nth-child(1) p:nth-child(1)");
+playerOneScoreDisplay.innerText = `Player One: ${playerOne.name}`;
+let playerTwoScoreDisplay = document.querySelector("#score-displays div:nth-child(2) p:nth-child(1)");
+playerTwoScoreDisplay.innerText = `Player Two: ${playerTwo.name}`;
 
-    gameFlow.playersTurn();
+gameFlow.playersTurn();
 
-    playerForm.close();
-    e.preventDefault();
+playerForm.close();
+e.preventDefault();
+});
+
+let restartButton = document.querySelector("#restart-button");
+restartButton.addEventListener("click", () => {
+for(let i = 0; i < spaces.length; i++) { 
+    spaces[i].innerHTML = "";
+}
+gameBoard.restartBoard();
 });
 
 let spaces = document.querySelectorAll("td");
 for(let i = 0; i < spaces.length; i++) {
     spaces[i].addEventListener("click", (e) => {
-        console.log(gameFlow.gameOver);
-        let img = document.createElement("img");
-        if(gameFlow.playersTurn() === playerOne) {
-            img.setAttribute("src", "images\\o.png");
-            gameBoard.updateBoard(Math.floor(i/3), i%3, playerOne);
-        } else {
-            img.setAttribute("src", "images\\x.png");
-            gameBoard.updateBoard(Math.floor(i/3), i%3, playerTwo);
-        }
-        spaces[i].appendChild(img);
+        gameBoard.updateBoard(Math.floor(i/3), i%3, gameFlow.playersTurn(), spaces[i]);
     });
 }
 
-let restartButton = document.querySelector("#restart-button");
-restartButton.addEventListener("click", () => {
-    gameBoard.restartBoard();
-    for(let i = 0; i < spaces.length; i++) { 
-        spaces[i].innerHTML = "";
-    }
-});
+let winDisplay = document.querySelector("#display-win");
